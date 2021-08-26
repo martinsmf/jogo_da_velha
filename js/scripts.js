9d09c31b6f7a41af7d2f30094ae7d134b213fd5d
Matheus Martins <matheus.martins@rarolabs.com>
2021-08-25 23:49:16 -0300

    logica vitória primeira parte

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,8 @@ for (let i = 0; i < boxes.length; i++) {
             } else {
                 player02++
             }
+
+            checkWinCondition();
         }
 
     })
@@ -47,3 +49,48 @@ function checkEl(player01, player02) {
     }
     return el;
 }
+
+
+function checkWinCondition() {
+    let b1 = document.getElementById('block-1');
+    let b2 = document.getElementById('block-2');
+    let b3 = document.getElementById('block-3');
+    let b4 = document.getElementById('block-4');
+    let b5 = document.getElementById('block-5');
+    let b6 = document.getElementById('block-6');
+    let b7 = document.getElementById('block-7');
+    let b8 = document.getElementById('block-8');
+    let b9 = document.getElementById('block-9');
+
+    /* const horizontal = win['horizontal'];
+    horizontal(b1, b2, b3); */
+
+    if (b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0) {
+        let b1child = b1.childNodes[0].className;
+        let b2child = b2.childNodes[0].className;
+        let b3child = b3.childNodes[0].className;
+
+        if (b1child == 'x' && b2child == 'x' && b3child == 'x') {
+            console.log('x venceu');
+
+        } else if (b1child == 'o' && b2child == 'o' && b3child == 'o') {
+            console.log('o venceu');
+        }
+    }
+}
+
+/* const win = {
+    horizontal: (b1, b2, b3) => {
+
+        let b1child = b1.childNodes[0].className;
+        let b2child = b2.childNodes[0].className;
+        let b3child = b3.childNodes[0].className;
+
+        if (b1child == 'x' && b2child == 'x' && b3child == 'x') {
+            console.log('x venceu');
+
+        } else if (b1child == 'o' && b2child == 'o' && b3child == 'o') {
+            console.log('o venceu');
+        }
+    }
+} */
